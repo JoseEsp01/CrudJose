b1def2cc05f021d4af82b4cdeddc7878fc8f509a
JoseEsp01 <jose.espinoza.est@tecazuay.edu.ec>
2023-02-25 23:34:01 -0500

    cambios

--- src/main/java/com/as/crud/CrudApplication.java
+++ src/main/java/com/as/crud/CrudApplication.java
@@ -3,6 +3,9 @@ package com.as.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -19,6 +22,16 @@ public class CrudApplication {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("https://hg-rest-api.herokuapp.com/api/products/product/").allowedMethods("").allowedHeaders("*");
+            }
+        };
     }
 
     public static void main(String[] args) {
